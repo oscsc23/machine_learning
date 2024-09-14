2c40198f8dd7c2da4b5cd99b9cd2d7a024fad9dc
oscsc23 <139048001+oscsc23@users.noreply.github.com>
2024-09-14 15:45:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,21 @@ with st.sidebar:
   st.header('Input features')
   island = st.selectbox('Island', ('Biscoe','Dream','Torgersen'))
   gender = st.selectbox('Gender', ('male','female'))
-  bill_length = st.slider('Bill length (mm)',32.1,59.6,43.9)
+  bill_length_mm = st.slider('Bill length (mm)',32.1,59.6,43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172, 231, 201)
+  body_mass_g = st.slider('Body mass (g)', 2700,6300,4207)
+  gender = st.selectbox('Gender', ('male','female'))
+
+#Create a dataframe for the input features
+  data = ('Island, Island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g' : body_mass_g,
+          'gender' : gender)
+  input_df = pd.DataFrame(data, index[0])
+  input_penguins = pd.concat([input_df, X], axis=0)
+
+input_penguins
   
