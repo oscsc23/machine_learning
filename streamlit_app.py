ae871d2f9059ba4d07fffe93b24fbce5de32a5b6
oscsc23 <139048001+oscsc23@users.noreply.github.com>
2024-09-14 15:23:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('ML App')
 
 st.info('This app builds a machine learning model')
 
+with st.expander('Data':
+  st.write('**Raw Data')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df
