046286aa0a7535044ab58d8a3c3aa1fae72e062b
oscsc23 <139048001+oscsc23@users.noreply.github.com>
2024-09-14 15:25:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.info('This app builds a machine learning model')
 with st.expander('Data'):
   st.write('**Raw Data')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df
+df.head()
 
 st.write('**X')
 X = df.drop('species',axis=1)
