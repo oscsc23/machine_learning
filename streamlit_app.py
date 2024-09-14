aae27eb3da2f3938897d152dc017807acc1a1624
oscsc23 <139048001+oscsc23@users.noreply.github.com>
2024-09-14 15:24:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,15 @@ st.title('ML App')
 
 st.info('This app builds a machine learning model')
 
-with st.expander('Data':
+with st.expander('Data'):
   st.write('**Raw Data')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df
+
+st.write('**X')
+X = df.drop('species',axis=1)
+X
+
+st.write('**y**')
+y = df.species
+y
